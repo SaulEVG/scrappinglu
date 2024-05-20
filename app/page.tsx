2ce37c913bfc9f769dcb08892c5d6769b02375ec
SaulEVG <valdez.ernesto@gmail.com>
2024-05-20 17:55:40 -0300

    adding a more time fo function works

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ProductList from "./ui/components/ProductList";
 import SearchBar from "./ui/components/SearchBar";
 import ProductListSkeleton from "./ui/components/skeleton/ProductListSkeleton";
 import connectDB from "./db/database";
+export const maxDuration = 60;
 
 export default async function Home(searchParams: {
   params: {};
